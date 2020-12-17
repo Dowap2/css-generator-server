1bac0321b15ab7cdb7615d27fdb2a846fdd44c7d
dowap2 <dowapdowari@gmail.com>
2020-12-17 21:12:42 +0900

    modify file

--- app.js
+++ app.js
@@ -1,112 +1,90 @@
-var express = require("express");
 var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var app = express();
-var cors = require("cors");
-
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
-mongoose.connect("mongodb://localhost/generator", { useNewUrlParser: true });
+// 2. testDB 세팅
+mongoose.connect("mongodb://localhost:27017/testDB");
+// 3. 연결된 testDB 사용
 var db = mongoose.connection;
-
+// 4. 연결 실패
+db.on("error", function() {
+  console.log("Connection Failed!");
+});
+// 5. 연결 성공
 db.once("open", function() {
-  console.log("DB connected");
+  console.log("Connected!");
 });
 
-db.on("error", function(err) {
-  console.log("DB ERROR : ", err);
+// 6. Schema 생성. (혹시 스키마에 대한 개념이 없다면, 입력될 데이터의 타입이 정의된 DB 설계도 라고 생각하면 됩니다.)
+var student = mongoose.Schema({
+  name: "string",
+  address: "string",
+  age: "number"
 });
 
-app.use(cors());
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// 7. 정의된 스키마를 객체처럼 사용할 수 있도록 model() 함수로 컴파일
+var Student = mongoose.model("Schema", student);
 
-var boxSchema = mongoose.Schema({
-  boxWidth: { type: Number },
-  boxHeight: { type: Number },
-  borderWidth: { type: Number },
-  borderStyle: { type: String },
-  borderColor: { type: String },
-  colorType: { type: String },
-  boxColor: { type: String },
-  color1: { type: String },
-  color2: { type: String },
-  gradientStyle: { type: String },
-  deg: { type: Number },
-  transformX: { type: Number },
-  transformY: { type: Number },
-  rotateX: { type: Number },
-  rotateY: { type: Number },
-  rotateZ: { type: Number },
-  text: { type: String },
-  textColor: { type: String },
-  textStyle: { type: String },
-  fontSize: { type: Number }
+// 8. Student 객체를 new 로 생성해서 값을 입력
+var newStudent = new Student({
+  name: "Hong Gil Dong",
+  address: "서울시 강남구 논현동",
+  age: "22"
 });
 
-var Contact = mongoose.model("contacts", boxSchema);
-
-app.get("/api", function(req, res) {
-  Contact.find({}, function(err, contacts) {
-    if (err) {
-      return res.json(err);
-    }
-    res.send({ contact: contacts });
-  });
+// 9. 데이터 저장
+newStudent.save(function(error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Saved!");
+  }
 });
 
-app.post("/api", function(req, res) {
-  Contact.create(req.body, function(err, contact) {
-    if (err) {
-      return console.log(err);
-    } else {
-    }
-  });
+// 10. Student 레퍼런스 전체 데이터 가져오기
+Student.find(function(error, students) {
+  console.log("--- Read all ---");
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(students);
+  }
 });
 
-app.put("/api", function(req, res) {
-  Contact.update(
-    { _id: req.body.params._id },
-    { $set: { title: req.body.data.title, main: req.body.data.main } },
-    function(err) {
-      if (err) {
-        return console.log("error");
+// 11. 특정 아이디값 가져오기
+Student.findOne({ _id: "585b777f7e2315063457e4ac" }, function(error, student) {
+  console.log("--- Read one ---");
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(student);
+  }
+});
+
+// 12. 특정아이디 수정하기
+Student.findById({ _id: "585b777f7e2315063457e4ac" }, function(error, student) {
+  console.log("--- Update(PUT) ---");
+  if (error) {
+    console.log(error);
+  } else {
+    student.name = "--modified--";
+    student.save(function(error, modified_student) {
+      if (error) {
+        console.log(error);
       } else {
-        console.log(req);
+        console.log(modified_student);
       }
-    }
-  );
+    });
+  }
 });
 
-app.delete("/api", function(req, res) {
-  Contact.deleteOne({ _id: req.body.id }, function(err) {
-    if (err) {
-      return console.log("error");
-    } else {
-      console.log(req);
-    }
-  });
-});
+// 13. 삭제
+Student.remove({ _id: "585b7c4371110029b0f584a2" }, function(error, output) {
+  console.log("--- Delete ---");
+  if (error) {
+    console.log(error);
+  }
 
-app.put("/api/like", function(req, res) {
-  Contact.update(
-    { _id: req.body.params._id },
-    { $set: { like: req.body.data.like, bad: req.body.data.bad } },
-    function(err) {
-      if (err) {
-        return console.log(err);
-      } else {
-        console.log(req);
-      }
-    }
-  );
-});
-
-var port = 8000;
-
-app.listen(port, function() {
-  console.log("server on! http://localhost:" + port);
+  /* ( SINCE DELETE OPERATION IS IDEMPOTENT, NO NEED TO SPECIFY )
+        어떤 과정을 반복적으로 수행 하여도 결과가 동일하다. 삭제한 데이터를 다시 삭제하더라도, 존재하지 않는 데이터를 제거요청 하더라도 오류가 아니기 때문에
+        이부분에 대한 처리는 필요없다. 그냥 삭제 된것으로 처리
+        */
+  console.log("--- deleted ---");
 });
